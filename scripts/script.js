dcafe8fe764cce8bb220d54d0af501b1f328cce5
inkblotty <inkblot.and.blush@gmail.com>
2015-11-03 11:04:10 -0700

    fixed id resume object and main.js for popOver function

---
+++ scripts/script.js
@@ -0,0 +1,1 @@
+function popOver(e){e.style.display="block"}var resumeLink=document.getElementById("resume-link"),resumeObj=document.getElementById("resume");resumeLink.onclick=function(){popOver(resumeObj)};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,5 @@ var resumeLink = document.getElementById('resume-link');
 var resumeObj = document.getElementById('resume');
 
 resumeLink.onclick = function(){
-	console.log("I'm clicked! fo sho");
     popOver(resumeObj);
 };
