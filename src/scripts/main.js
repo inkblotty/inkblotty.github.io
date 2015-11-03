64924551200448eb40c2289b74e48689fd534b19
inkblotty <inkblot.and.blush@gmail.com>
2015-11-03 12:07:33 -0700

    took out resume link until resume is filled out

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 // JavaScript stuff
 
+var resumeLink = document.getElementById('resume-link');
+var resumeObj = document.getElementById('resume');
+
 function popOver(element) {
 	element.style.display = "block";
 }
-
-var resumeLink = document.getElementById('resume-link');
-var resumeObj = document.getElementById('resume');
 
 resumeLink.onclick = function(){
     popOver(resumeObj);
