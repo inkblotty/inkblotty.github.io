bd56bad0e04efc6091f8f22e28e0383165ce24c7
inkblotty <inkblot.and.blush@gmail.com>
2016-01-26 07:25:44 -0700

    tweaked Frameworks and Technologies section, added iframe with embedded livestream from livecoding but commented out for later formatting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,7 +15,7 @@ resumeLink.onclick = function(){
 $("a[href^='#']").click(function(e) {
 	e.preventDefault();
 	var dest = $(this).attr('href');
-	console.log(dest);
+	//console.log(dest);
 	$('html,body').animate({
 		scrollTop: $(dest).offset().top-100}, 'slow');
 });
