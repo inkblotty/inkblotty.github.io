2727dfa0eaec16caad912633c5780058fcbf4493
Katie McCormick <inkblotty@github.com>
2023-11-20 17:54:29 +0000

    trying out svelte

--- scripts/script.js
+++
@@ -1,1 +0,0 @@
-$("a[href^='#']").click(function(t){t.preventDefault();var o=$(this).attr("href");$("html,body").animate({scrollTop:$(o).offset().top-100},"slow")}),$.ajax({async:!0,contentType:"application/json",crossDomain:!0,url:"http://lctva.joel.io/api/online/inkblotty/",method:"GET",success:function(){},complete:function(t,o){}}).done(function(t){});
